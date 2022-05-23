eb335d358b2eadd8fd1886f7dfb8748dbeeb3462
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-05-23 16:55:26 +0800

    initial commit

---
+++ Beginning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beginning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+
+}
